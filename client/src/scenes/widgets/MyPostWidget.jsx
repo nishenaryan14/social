816faff46814b7ba9669ea7a153ffab422e6a86d
nishenaryan14 <aryannishen27@gmail.com>
2024-08-09 14:34:30 +0530

    image upload url

--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -47,8 +47,7 @@ const MyPostWidget = ({ picturePath }) => {
     }
 
     try {
-      // const response = await fetch(`https://social-ty3k.onrender.com/posts`, {
-      const response = await fetch(`http://localhost:3001/posts`, {
+      const response = await fetch(`https://social-ty3k.onrender.com/posts`, {
         method: "POST",
         headers: { Authorization: `Bearer ${token}` },
         body: formData,
