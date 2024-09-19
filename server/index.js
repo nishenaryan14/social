39509fc734914c062cd19c7aba08e00945d6c428
nishenaryan14 <aryannishen27@gmail.com>
2024-09-20 01:36:36 +0530

    cors origin

--- server/index.js
+++ server/index.js
@@ -52,7 +52,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", // Replace with your client origin
+    origin: "https://social-ty3k.onrender.com", // Replace with your client origin
     methods: ["GET", "POST"],
     allowedHeaders: ["my-custom-header"],
     credentials: true,
